647f61434f78f32df4a821bff079a0727be0bd2d
ClaudioCesarGobbi <claudiocesargobbi@gmail.com>
2025-09-28 23:22:09 -0300

    Implemente metodos de ClienteNegocio y VoucherNegocio: agregarCliente() con validacion de documento unico, modificarCliente() con validacion de documento unico, obtenerClientePorDocumento() para busqueda por DNI, obtenerVoucherPorCodigo() para verificar existencia, canjearVoucher() con registro de fecha de canje

--- TPWeb-Equipo-10A/Negocio/ClienteNegocio.cs
+++ TPWeb-Equipo-10A/Negocio/ClienteNegocio.cs
@@ -11,32 +11,162 @@ namespace Negocio
     {
         public int agregarCliente(Cliente clienteNuevo)
         {
-            // logica para agregar cliente
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (existeClientePorDocumento(clienteNuevo.Documento))
+                {
+                    throw new Exception("Ya existe un cliente con ese documento.");
+                }
+
+                datos.SetearConsulta("INSERT INTO Clientes (Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP) VALUES (@Documento, @Nombre, @Apellido, @Email, @Direccion, @Ciudad, @CP); SELECT SCOPE_IDENTITY();");
+                datos.SetearParametro("@Documento", clienteNuevo.Documento);
+                datos.SetearParametro("@Nombre", clienteNuevo.Nombre);
+                datos.SetearParametro("@Apellido", clienteNuevo.Apellido);
+                datos.SetearParametro("@Email", clienteNuevo.Email);
+                datos.SetearParametro("@Direccion", clienteNuevo.Direccion);
+                datos.SetearParametro("@Ciudad", clienteNuevo.Ciudad);
+                datos.SetearParametro("@CP", clienteNuevo.CodigoPostal);
+
+                datos.EjecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    return Convert.ToInt32(datos.Lector[0]);
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
 
         public void modificarCliente(Cliente clienteModificado)
         {
-            // logica para modificar cliente
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (existeClientePorDocumentoExcluyendoId(clienteModificado.Documento, clienteModificado.IdCliente))
+                {
+                    throw new Exception("Ya existe otro cliente con ese documento.");
+                }
+
+                datos.SetearConsulta("UPDATE Clientes SET Documento=@Documento, Nombre=@Nombre, Apellido=@Apellido, Email=@Email, Direccion=@Direccion, Ciudad=@Ciudad, CP=@CP WHERE Id=@Id");
+                datos.SetearParametro("@Documento", clienteModificado.Documento);
+                datos.SetearParametro("@Nombre", clienteModificado.Nombre);
+                datos.SetearParametro("@Apellido", clienteModificado.Apellido);
+                datos.SetearParametro("@Email", clienteModificado.Email);
+                datos.SetearParametro("@Direccion", clienteModificado.Direccion);
+                datos.SetearParametro("@Ciudad", clienteModificado.Ciudad);
+                datos.SetearParametro("@CP", clienteModificado.CodigoPostal);
+                datos.SetearParametro("@Id", clienteModificado.IdCliente);
+
+                datos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
 
         public Cliente obtenerClientePorDocumento(string documento)
         {
-            // logica para obtener cliente por documento
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP FROM Clientes WHERE Documento = @Documento");
+                datos.SetearParametro("@Documento", documento);
+                datos.EjecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    Cliente aux = new Cliente();
+                    aux.IdCliente = (int)datos.Lector["Id"];
+                    aux.Documento = (string)datos.Lector["Documento"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Apellido = (string)datos.Lector["Apellido"];
+                    aux.Email = (string)datos.Lector["Email"];
+                    aux.Direccion = (string)datos.Lector["Direccion"];
+                    aux.Ciudad = (string)datos.Lector["Ciudad"];
+                    aux.CodigoPostal = (int)datos.Lector["CP"];
+
+                    return aux;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
 
         private bool existeClientePorDocumento(string documento)
         {
-            // validacion de existencia por documento
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.SetearConsulta("SELECT COUNT(*) FROM Clientes WHERE Documento = @Documento");
+                datos.SetearParametro("@Documento", documento);
+                datos.EjecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    return (int)datos.Lector[0] > 0;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
 
         private bool existeClientePorDocumentoExcluyendoId(string documento, int idExcluir)
         {
-            // validacion de existencia excluyendo ID, para no duplicar documento
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.SetearConsulta("SELECT COUNT(*) FROM Clientes WHERE Documento = @Documento AND Id != @Id");
+                datos.SetearParametro("@Documento", documento);
+                datos.SetearParametro("@Id", idExcluir);
+                datos.EjecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    return (int)datos.Lector[0] > 0;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
     }
 }
--- TPWeb-Equipo-10A/Negocio/VoucherNegocio.cs
+++ TPWeb-Equipo-10A/Negocio/VoucherNegocio.cs
@@ -11,14 +11,87 @@ namespace Negocio
     {
         public Voucher obtenerVoucherPorCodigo(string codigoVoucher)
         {
-            // logica para obtener voucher por codigo
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT V.CodigoVoucher, V.IdCliente, V.FechaCanje, V.IdArticulo, C.Nombre, C.Apellido, C.Email, A.Nombre as NombreArticulo FROM Vouchers V LEFT JOIN Clientes C ON V.IdCliente = C.Id LEFT JOIN ARTICULOS A ON V.IdArticulo = A.Id WHERE V.CodigoVoucher = @CodigoVoucher");
+                datos.SetearParametro("@CodigoVoucher", codigoVoucher);
+                datos.EjecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    Voucher aux = new Voucher();
+                    aux.CodigoVoucher = (string)datos.Lector["CodigoVoucher"];
+
+                    if (datos.Lector["FechaCanje"] != DBNull.Value)
+                    {
+                        aux.FechaCanje = (DateTime)datos.Lector["FechaCanje"];
+                    }
+
+                    if (datos.Lector["IdCliente"] != DBNull.Value)
+                    {
+                        aux.Cliente = new Cliente();
+                        aux.Cliente.IdCliente = (int)datos.Lector["IdCliente"];
+                        aux.Cliente.Nombre = (string)datos.Lector["Nombre"];
+                        aux.Cliente.Apellido = (string)datos.Lector["Apellido"];
+                        aux.Cliente.Email = (string)datos.Lector["Email"];
+                    }
+
+                    if (datos.Lector["IdArticulo"] != DBNull.Value)
+                    {
+                        aux.Articulo = new Articulo();
+                        aux.Articulo.IdArticulo = (int)datos.Lector["IdArticulo"];
+                        aux.Articulo.NombreArticulo = (string)datos.Lector["NombreArticulo"];
+                    }
+
+                    return aux;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
 
         public void canjearVoucher(string codigoVoucher, int idCliente, int idArticulo)
         {
-            // logica para canjear voucher
-            throw new NotImplementedException();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                Voucher voucher = obtenerVoucherPorCodigo(codigoVoucher);
+                if (voucher == null)
+                {
+                    throw new Exception("El voucher no existe.");
+                }
+
+                if (voucher.Usado)
+                {
+                    throw new Exception("El voucher ya fue utilizado.");
+                }
+
+                datos.SetearConsulta("UPDATE Vouchers SET IdCliente = @IdCliente, FechaCanje = @FechaCanje, IdArticulo = @IdArticulo WHERE CodigoVoucher = @CodigoVoucher");
+                datos.SetearParametro("@IdCliente", idCliente);
+                datos.SetearParametro("@FechaCanje", DateTime.Now);
+                datos.SetearParametro("@IdArticulo", idArticulo);
+                datos.SetearParametro("@CodigoVoucher", codigoVoucher);
+                datos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
     }
 }
